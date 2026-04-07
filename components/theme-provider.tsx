a4deeb5af9a4ba93fa5e42b6a8354bce38d074bb
MyMedbook <iskender.n2k3@gmail.com>
2026-04-08 02:16:40 +0400

    feat: add theme provider and toggle components for dark mode support

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -11,7 +11,6 @@ export default function ThemeProvider({
     <NextThemesProvider
       attribute="class"
       defaultTheme="system"
-      enableSystem
       disableTransitionOnChange={false}
     >
       {children}
